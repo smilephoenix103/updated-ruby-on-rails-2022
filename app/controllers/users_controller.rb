69d18e88a60c1e55ff6999315e1982d7a95488c3
smilephoenix103 <marugotomoaki@gmail.com>
2022-04-26 18:14:56 +0200

    added redirect to show page in case of valid form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # to be completed
+      redirect_to @user
     else
       render 'new'
     end
