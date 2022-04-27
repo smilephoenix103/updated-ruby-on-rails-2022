44f8b83c2b862efb21585608830f8ab24ea4e0e5
smilephoenix103 <marugotomoaki@gmail.com>
2022-04-27 23:24:37 +0200

    added test for successful edit and completed update action (valid form submission branch)

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,4 +18,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors', count: 8
   end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'foo bar'
+    email = 'bar@foo.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: '',
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
