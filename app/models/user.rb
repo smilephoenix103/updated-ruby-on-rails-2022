8cc7e383cd99262219849ccd87976ef0c8722ec3
smilephoenix103 <marugotomoaki@gmail.com>
2022-04-27 11:21:35 +0200

    added test for valid login as well as invalid login due to wrong password only

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,10 +2,37 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-  test "invalid login flash notice" do
+  def setup
+    @user = users(:jerome)
+  end
+
+  test 'invalid login flash notice' do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: '', password: '' } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test 'valid login' do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test 'login with valid email / invalid password' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: 'a@a.com',
+                                          password: 'invalid' } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
