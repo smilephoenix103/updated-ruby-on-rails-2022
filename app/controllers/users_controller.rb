a95bc175646c6b91d4264431d20e60429609f53b
smilephoenix103 <marugotomoaki@gmail.com>
2022-04-28 10:21:18 +0200

    added redirection from edit / update to home page for logged in but wrong users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %w[edit update]
+  before_action :correct_user, only: %w[edit update]
 
   def show
     @user = User.find(params[:id])
@@ -43,10 +44,17 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
+  # confirms a logged in user.
   def logged_in_user
     unless logged_in?
       flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
+  end
+
+  # confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
