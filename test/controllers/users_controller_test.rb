07943621608997d6a5cb58c42d094d0040a27719
smilephoenix103 <marugotomoaki@gmail.com>
2022-04-28 10:07:50 +0200

    created a test to check that udpate is redirected to home page if logged-in but wrong user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -30,4 +30,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test 'should redirect update to home page if logged in as wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
