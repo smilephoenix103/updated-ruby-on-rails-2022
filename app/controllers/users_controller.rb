d6eedef1f17bdc611a4768429b164a1a4c99d5b6
smilephoenix103 <marugotomoaki@gmail.com>
2022-04-28 16:11:17 +0200

    amended user controller's create action to have the account activation mail sent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:success] = "An account activation link has been sent
+      to #{@user.email}. Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
