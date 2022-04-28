50354faa35d8ec0471a5d8ac3ab92de411228541
smilephoenix103 <marugotomoaki@gmail.com>
2022-04-28 13:28:16 +0200

    added tests on redirecting destroy for not logged in or non admin users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %w[index edit update]
+  before_action :logged_in_user, only: %w[index edit update destroy]
   before_action :correct_user, only: %w[edit update]
   before_action :admin_user, only: :destroy
 
@@ -72,6 +72,6 @@ class UsersController < ApplicationController
 
   # confirms the user has an admin role.
   def admin_user
-    redirect_to root_path unless current_user.admin?
+    redirect_to(root_url) unless current_user.admin?
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:jerome)
     @other_user = users(:kevin)
@@ -36,12 +35,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
-    assert_redirected_to root_url
+    assert_redirected_to root_path
   end
 
   test 'should redirect index when not logged in' do
     get users_path
-    assert_redirected_to login_url
+    assert_redirected_to login_path
   end
 
   test 'changing a user to admin using patch method via the web should not be allowed' do
@@ -52,5 +51,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password",
                                             admin: true } }
     assert_not @other_user.reload.admin?
+  end
+
+  test 'should redirect destroy to login when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy to home page when not admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
   end
 end
